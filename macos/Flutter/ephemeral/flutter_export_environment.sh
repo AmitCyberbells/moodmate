45863d75d864b9cbcb661b786df09b24a4a5fb05
LovejeetSingCyberbells <lovejeet.cyberbells@gmail.com>
2025-09-02 13:54:46 +0530

    create structure

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=L:\flutter sdk\flutter"
-export "FLUTTER_APPLICATION_PATH=L:\Cyberbells\moodmate"
+export "FLUTTER_APPLICATION_PATH=L:\Cyberbells\moodmate\moodmate"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
